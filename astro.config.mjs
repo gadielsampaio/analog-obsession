43f39073d604b0d840372d251da5c3e6d20c7c64
gadielsampaio <marcosgadielsampaio@gmail.com>
2026-08-19 16:25:07 -0400

    fix: route

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   trailingSlash: 'never',
+  site: 'https://gadielsampaio.github.io/',
+  base: '/analog-obsession',
   build: {
     format: 'file',
     inlineStylesheets: 'never',
